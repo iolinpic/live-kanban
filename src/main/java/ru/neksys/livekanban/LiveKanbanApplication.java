7f2c884d96ef9b75ac57b8a467e758dc90ab9dc1
iolinpic <iolinpic@gmail.com>
2026-01-15 20:20:53 +0300

    initial commit

---
+++ src/main/java/ru/neksys/livekanban/LiveKanbanApplication.java
@@ -0,0 +1,13 @@
+package ru.neksys.livekanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveKanbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveKanbanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/neksys/livekanban/LiveKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neksys.livekanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveKanbanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
